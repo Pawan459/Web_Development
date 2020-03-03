45337d39d6686f081f8333d2ca1614926d737363
pawan459 <pawan18092019@gmail.com>
2020-03-03 21:27:59 +0530

    update

--- StickyHeaders/index.js
+++ StickyHeaders/index.js
@@ -1,13 +1,17 @@
 const eleBody = document.body
-let headerCount = 0
-
-const headerEvent = (event) =>{
-    console.log(event)
-}
+let headerCount = 0, currTop = undefined, divIndex = undefined, scrollDown = true
+let mainDivArray = []
 
 const makeContent = () => {
     let mainDiv = document.createElement('div')
     mainDiv.className = 'container'
+
+    let header = document.createElement('div')
+    header.className = 'header'
+    header.id = `header${headerCount++}`
+    header.innerHTML = 'Sticky Thing'
+
+    mainDiv.appendChild(header)
 
     for(let i = 0;i<6;i++){
         let div = document.createElement('div')
@@ -16,15 +20,7 @@ const makeContent = () => {
         div.innerHTML = i
         mainDiv.appendChild(div)
     }
-
-    let header = document.createElement('div')
-    header.className = 'header'
-    header.id = `header${headerCount++}`
-    header.innerHTML = 'Sticky Thing'
-    header.addEventListener('scroll', headerEvent)
-
-    mainDiv.appendChild(header)
-    console.log(mainDiv)
+    mainDivArray.push(mainDiv)
     return mainDiv
 }
 
@@ -32,13 +28,61 @@ const renderInit = (event) => {
     for(let i = 0;i<6;i++){
         eleBody.insertBefore(makeContent(), eleBody.firstChild)
     }
+    divIndex = mainDivArray.length - 1
+    currTop = mainDivArray[divIndex].getBoundingClientRect().top
 }
 
-const makeCurrentHeaderSticky = (event) => {
-    let headers = document.getElementsByClassName('header')
+const visibleElement = (element) => {
+    // Now I have to check whether the currTop is greater than bottom of currentDiv
+    let eleRect = element.getBoundingClientRect()
+    let bottom = eleRect.bottom
+    console.log(currTop, bottom)
     
+    if(scrollDown) return currTop <= bottom
+    return currTop >= bottom
+}
+
+const scrollWindowEvent = (event) => {
+    console.log(divIndex)
+    if(scrollDown){
+        if (!mainDivArray[divIndex]) return
+
+        let header = mainDivArray[divIndex].getElementsByClassName('header')[0]
+        if (visibleElement(mainDivArray[divIndex])) {
+            header.classList.add('sticky')
+        }
+        else {
+            header.classList.remove('sticky')
+            divIndex--
+            if (divIndex == -1) {
+                scrollDown = false
+                divIndex = 0
+            }
+            if (!mainDivArray[divIndex]) return
+            currTop = mainDivArray[divIndex].getBoundingClientRect().top
+        }
+
+    }
+    else{
+        if (!mainDivArray[divIndex]) return
+
+        let header = mainDivArray[divIndex].getElementsByClassName('header')[0]
+        if (visibleElement(mainDivArray[divIndex])) {
+            header.classList.add('sticky')
+        }
+        else {
+            header.classList.remove('sticky')
+            divIndex++
+            if (divIndex == mainDivArray.length) {
+                divIndex -= 1
+                scrollDown = true
+            }
+            if (!mainDivArray[divIndex]) return
+            currTop = mainDivArray[divIndex].getBoundingClientRect().top
+        }
+    }
 }
 
 window.addEventListener('load', renderInit)
 
-window.addEventListener('scroll', makeCurrentHeaderSticky)
+window.addEventListener('scroll', scrollWindowEvent)
